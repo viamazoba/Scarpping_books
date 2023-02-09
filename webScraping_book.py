33c531b40b485b05fb1e24de7c990e32607312ac
viamazoba <viamazoba@unal.edu.co>
2023-02-08 21:51:03 -0500

    Se añande el método para obtener la descripción de los libros, además, se establece método para cerrar el browser

--- webScraping_book.py
+++ webScraping_book.py
@@ -24,6 +24,7 @@ class webScraping_book():
         self._prices = []
         self._stars = []
         self._states = []
+        self._description = []
     
      # En el siguiente método cargas la página web
     def cargar_page(self):
@@ -74,10 +75,40 @@ class webScraping_book():
         tabs = self._driver.window_handles
         # Seleccionas la nueva ventana abierta para trabajar
         self._driver.switch_to.window(tabs[-1])
+
+    
+     
+    """El método a continuación obtiene la descripción del libro al entregarle la dirección a su HTML"""
+    def book_description(self, url):
+
+        # Aquí abres una nueva ventana con la referencia que obtuviste
+        self._driver.execute_script("window.open('{}');".format(url))
+        # Obtienes las pestañas abiertas
+        tabs = self._driver.window_handles
+        # Seleccionas la nueva ventana abierta para trabajar
+        self._driver.switch_to.window(tabs[-1])
+
+        product = self._driver.find_elements(By.TAG_NAME, 'article')
+        tag_description = product[0].find_element(By.XPATH, '//*[@id="content_inner"]/article/p')
+        description = tag_description.text
+        print(description) 
+
+        # Se cierra la ventana
+        self._driver.close()
+        # Regresamos a la ventana anterior (ventana principal)
+        self._driver.switch_to.window(self._driver.window_handles[0])
+
+
+
+        return description 
+
     
     def obtain_info_books(self):
         # Se obtienen todos los artículos visibles en la página
         articles = self._driver.find_elements(By.TAG_NAME, 'article')
+
+
+        # Las siguientes líneas hasta el print las puedes borrar si no vas hacer más pruebas
         tag_star = articles[0].find_element(By.CLASS_NAME, 'star-rating')
         #title = tag_principal_title.find_element(By.TAG_NAME, 'a')
         print(tag_star.get_attribute('class').split(' ')[1])
@@ -89,19 +120,33 @@ class webScraping_book():
             tag_principal_title = book.find_element(By.TAG_NAME, 'h3')
             title = tag_principal_title.find_element(By.TAG_NAME, 'a')
 
+            # Se toma la dirección del HTML del libro, esto para obtener la descripción del mismo
+            html_book = title.get_attribute('href')
+            self._description.append(self.book_description(html_book))
             # A continuación se hace algo similar para obtener el rating de los libros
             tag_star = book.find_element(By.CLASS_NAME, 'star-rating')
 
             self._titles.append(title.get_attribute('title'))
             self._prices.append(book.text.split('\n')[1])
             self._stars.append(tag_star.get_attribute('class').split(' ')[1])
-            self._states.append(book.text.split('\n')[2])
+            self._states.append(book.text.split('\n')[2]) 
 
-        print(self._titles[0])
-        print(self._prices[0])
-        print(self._stars[0])
-        print(self._states[0])
     
+    """En el siguiente método se le da click() a next para pasar a la siguiente página"""
+    def next_page(self):
+
+        elements = self._driver.find_elements(By.CLASS_NAME, 'next')
+        # Se establece una condición por si no existe el elemento next (última página)
+        if elements:
+            next_button = elements[0].find_element(By.TAG_NAME, 'a')
+            next_button.click()
+
+    """Cierras el browser"""
+    def close_web(self):
+        # Se cierra la ventana
+        self._driver.close()
+
+   
 
 if __name__ == "__main__":
 
@@ -114,5 +159,11 @@ if __name__ == "__main__":
     generos , referencias = library.obtain_genres()
     #print(referencias)
     library.obtain_info_books()
+    library.next_page()
+    library.close_web()
     #library.click_selected_genre() 
     #print('resultados nuevos:',library.obtain_results())
+
+
+    """Falta agregarle al método obtain_info_books un if, en el cual si una variable global está en True, este va a obtener además la descrpción de cada 
+    uno de los libros, esta variable global se establece con el GUI de la palicación."""
